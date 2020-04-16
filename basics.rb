cd6fdf0f96122670516065344af31b9ccaca160d
Grant Holley <gvholley@gmail.com>
2020-04-16 23:03:39 +0900

    Ruby Basics

---
+++ basics.rb
@@ -0,0 +1,19 @@
+age = 32
+# Assign the value 32 to the variable age
+
+
+age = (age + 1)
+#reassigns the variable age
+
+puts age
+
+first_name = "kaori"
+last_name = "miyata"
+
+#Concatenation
+puts first_name + " " + last_name
+
+#Interpolation
+puts "#{first_name} #{last_name}"
+
+#\n = new line inside string
